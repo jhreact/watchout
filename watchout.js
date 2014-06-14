42c518f0c3b1941073159a8763e6c8082e37d0aa
jhreact <jhreact@users.noreply.github.com>
2014-06-13 21:59:32 -0700

    Added numPoints to gameoptions Changed data from fake data to getNewPoints (with optional parameter) Update gets coords internally, instead of being passed

--- watchout.js
+++ watchout.js
@@ -12,16 +12,8 @@
 var gameOptions = {
   width: 1900,
   height: 1500,
+  numPoints: 100,
   scalingFactor: 1000
-};
-
-var _getFakeData = function () {
-  var dataArray = [];
-  for (var i = 0; i < 100; i++) {
-    dataArray.push({x: Math.random() * gameOptions.scalingFactor,
-      y: Math.random() * gameOptions.scalingFactor});
-  }
-	return dataArray;
 };
 
 
@@ -31,8 +23,19 @@ var board = d3.select('body')
   .attr('width', gameOptions.width)
   .attr('height', gameOptions.height);
 
+var _getNewPoints = function(numPoints) {
+  var dataArray = [];
+  numPoints = numPoints || gameOptions.numPoints;
+  for (var i = 0; i < numPoints; i++) {
+    dataArray.push({x: Math.random() * gameOptions.scalingFactor,
+      y: Math.random() * gameOptions.scalingFactor});
+  }
+	return dataArray;
+};
 
-var update = function(coords)   {
+
+var update = function()   {
+  var coords = _getNewPoints();
   var enemies = board.selectAll('circle').data(coords)
   enemies.enter().append('circle')
     .attr('cx', function(d) {
@@ -44,8 +47,8 @@ var update = function(coords)   {
     .attr('r', 10);
 };
 
-update(_getFakeData());
 
+update();
 
 
 
