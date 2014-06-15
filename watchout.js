3a7788ac1d5c951637f22bf1e8397eb06a67d693
jhreact <jhreact@users.noreply.github.com>
2014-06-15 11:53:40 -0700

    detects collision...w00t!

--- watchout.js
+++ watchout.js
@@ -1,16 +1,34 @@
 var gameOptions = {
   width: window.innerWidth,
   height: window.innerHeight,
-  numPoints: 10,
+  numPoints: 50,
   scalingFactor: 1000
 };
 
-var board = d3.select('body')
-  .append('svg')
-  .attr('width', gameOptions.width)
-  .attr('height', gameOptions.height);
+gameStats = {
+	currentScore: 0,
+	highScore: 0,
+	collisions: 0
+}
 
-var _getNewPoints = function(numPoints) {
+var board = d3.select('body').append('svg')
+      .attr('width', gameOptions.width)
+      .attr('height', gameOptions.height);
+
+//update the score
+var updateScore = function() {
+	return d3.select('.current').text("Score: " + gameStats.currentScore.toString());
+};
+
+//update the high score
+var updateHighScore = function() {
+	if (gameStats.score > gameStats.bestScore) {
+		gameStats.bestScore = gameStats.score;
+	}
+	return d3.select('.high').text("High score: " + gameStats.highScore.toString());
+};
+
+var _getCoords = function(numPoints) {
   var dataArray = [];
   numPoints = numPoints || gameOptions.numPoints;
   for (var i = 0; i < numPoints; i++) {
@@ -20,43 +38,34 @@ var _getNewPoints = function(numPoints) {
 	return dataArray;
 };
 
-var updateDots = function(coords)   {
-  coords = coords || _getNewPoints();
-  // BIND DATA
-  //
-  var enemies = board.selectAll('circle.enemy').data(coords);
+var enemies = board.selectAll('circle.enemy').data(_getCoords())
+  .enter().append('circle')
+  .attr('cx', function(d) { return d.x; })
+  .attr('cy', function(d) { return d.y; })
+  .attr('r', 10)
+  .attr('fill', 'blue')
+  .attr('class', 'enemy');
 
-  // UPDATE
-  enemies.transition().duration(1000).attr('cx', function(d) {
-      return d.x;
-    })
-    .attr('cy', function(d) {
-      return d.y;
-    });
-
-  // ENTER
-  enemies.enter().append('circle')
-    .attr('cx', function(d) {
-      return d.x;
-    })
-    .attr('cy', function(d) {
-      return d.y;
-    })
-    .attr('r', 10)
-    .attr('fill', 'blue')
-    .attr('class', 'enemy');
-
+var move = function(coords) {
+  coords = coords || _getCoords();
+  enemies.data(coords)
+    .transition()
+    .duration(1000)
+    .attr('cx', function(d) { return d.x; })
+    .attr('cy', function(d) { return d.y; });
+  detectCollisions();
+  updateScore();
+  updateHighScore();
 };
 
-setInterval(updateDots, 1000);
-// updateDots();
+setInterval(move, 1000);
 
 
 var dragMove = function(d) {
-  var mouse = d3.select(this);
-  mouse.attr('cx', d3.event.x)
+  d3.select(this)
+    .attr('cx', d3.event.x)
     .attr('cy', d3.event.y);
-  // detectCollisions(mouse);
+  detectCollisions();
 };
 
 var drag = d3.behavior.drag()
@@ -70,36 +79,34 @@ var player = board.append('circle')
   .attr('r', 10)
   .call(drag);
 
-// d3.select('.playerDot').call(drag);
 
-
-var detectCollisions = function(player) {
-  // var enemies = d3.selectAll('circle.enemy').some(function(enemy, enemyIdx, enemies) {
-  console.log(player.event.x);
-  var enemies = d3.selectAll('circle.enemy');
-  enemies.some(function(enemy, enemyID) {
-    // enemy = d3.select(this);
-    // console.log(enemy);
-  });
-  // console.log(enemies);
-  // enemies.some(function(enemy, enemyIdx, enemies) {
-  //   // console.log("ENEMY?");
-  //   // console.log(enemy);
-  //   if (intersects(d3.select('.playerDot')[0][0], enemy)) {
-  //     alert('WE HIT! BATTLESTATIONS!');
-  //   }
-  //
-};
-
-var intersects = function(player, enemy) {
-  console.log("Calling intersects!!!");
-  console.log(player);
-  var xDist = player.cx - enemy.cx;
-  var yDist = player.cy - enemy.cy;
+var intersects = function(x1, y1, r1, x2, y2, r2) {
+  var xDist = x1 - x2;
+  var yDist = y1 - y2;
   var dist = Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
-  // console.log(xDist, yDist, dist);
-  return dist < player.r + enemy.r;
+  return dist < r1 + r2;
 };
+
+// So, detects collisions, but only gets called when dragging mouse, or (I
+// beleve) at the endpoint of a transition. Need to figure out how to do the
+// calculations *during* the transition, though that'll probably be
+// computationally intensive
+var detectCollisions = function() {
+  var collision = false;
+  enemies.each(function() {
+    var enemy = d3.select(this);
+    var enemyX = enemy[0][0].cx.animVal.value;
+    var enemyY = enemy[0][0].cy.animVal.value;
+    var enemyR = enemy[0][0].r.animVal.value;
+    var playerX = player[0][0].cx.animVal.value;
+    var playerY = player[0][0].cy.animVal.value;
+    var playerR = player[0][0].r.animVal.value;
+    if (intersects(enemyX, enemyY, enemyR, playerX, playerY, playerR)) {
+      alert("BAM SON!");
+    }
+  });
+};
+
 
 
 
